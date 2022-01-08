75bfa32cc51454eeb82431eb7b871e4fb137f07d
mohamedbushara.169 <mohamedbushara.169@outlook.com>
2022-01-08 16:48:22 +0200

    8/1 => Last

---
+++ HR-OxCode/Models/Contracts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_OxCode.Models
+{
+    public class Contracts
+    {
+        public int id { get; set; }
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        //Contracts Details : 
+
+        public DateTime DateForm { get; set; }
+        public DateTime DateTo { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
