6e92c008b153c62dff06c28f6720428c14182f00
mohamedbushara.169 <mohamedbushara.169@outlook.com>
2022-03-03 14:22:31 +0200

    //////

---
+++ DXWebApplication2/Controllers/ReporttController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DXWebApplication2.Controllers
+{
+    public class ReporttController : Controller
+    {
+        public IActionResult IndexApprisal()
+        {
+            return View();
+        }
+    }
+}
